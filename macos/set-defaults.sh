794a23ef38c07b4d06d24727090c243c394fac87
Lidan Hifi <lidanh@wix.com>
2018-01-25 10:15:00 +0200

    update mac settings:

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -7,25 +7,114 @@
 #
 # Run ./set-defaults.sh and you'll be good to go.
 
-# Disable press-and-hold for keys in favor of key repeat.
-defaults write -g ApplePressAndHoldEnabled -bool false
 
-# Use AirDrop over every interface. srsly this should be a default.
-defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+# ==============================================
+# Files and folders
+# ==============================================
 
-# Set a really fast key repeat.
+# Show the ~/Library directory
+chflags nohidden "${HOME}/Library"
+
+# Don't show the ~/bin directory
+chflags hidden "${HOME}/bin"
+
+
+# ==============================================
+# NSGlobalDomain settings
+# ==============================================
+
+# Locale
+defaults write NSGlobalDomain AppleLocale -string "en_IL"
+defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
+defaults write NSGlobalDomain AppleMetricUnits -bool true
+
+# Use smooth scrolling
+defaults write NSGlobalDomain AppleScrollAnimationEnabled -bool true
+
+# Enable key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# Set keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 1
+
+
+# Audio and sound effects
+
+# Enable feedback when changing volume
+defaults write NSGlobalDomain com.apple.sound.beep.feedback -bool true
+
+
+# ==============================================
+# Mouse and trackpad
+# ==============================================
+
+# Tap to click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+
+# Tap with two fingers to emulate right click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
+
+# Disable "tap-and-a-half" to drag.
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Dragging -int 0
+defaults write com.apple.AppleMultitouchTrackpad Dragging -int 0
+
+# Enable 3-finger drag. (Moving with 3 fingers in any window)
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+
+# Enable force click
+defaults write NSGlobalDomain com.apple.trackpad.forceClick -bool true
+
+# Zoom in or out
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadPinch -bool true
+
+# Smart zoom, double-tap with two fingers
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadTwoFingerDoubleTapGesture -bool true
+
+# Rotate
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRotate -bool true
+
+# Notification Center
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadTwoFingerFromRightEdgeSwipeGesture -int 3
+
+
+# ==============================================
+# Xcode
+# ==============================================
+
+# Always use spaces for indenting
+defaults write com.apple.dt.Xcode DVTTextIndentUsingTabs -bool false
+
+# Show tab bar
+defaults write com.apple.dt.Xcode AlwaysShowTabBar -bool true
+
+
+# ==============================================
+# Finder
+# ==============================================
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
 # Set the Finder prefs for showing a few different volumes on the Desktop.
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
-# Run the screensaver if we're in the bottom-left hot corner.
-defaults write com.apple.dock wvous-bl-corner -int 5
-defaults write com.apple.dock wvous-bl-modifier -int 0
+
+# ==============================================
+# Safari & WebKit
+# ==============================================
+
+# Show status bar
+defaults write com.apple.Safari ShowStatusBar -bool true
 
 # Hide Safari's bookmark bar.
-defaults write com.apple.Safari ShowFavoritesBar -bool false
+defaults write com.apple.Safari ShowFavoritesBar -bool true
+defaults write com.apple.Safari "ShowFavoritesBar-v2" -bool true
 
 # Set up Safari for development.
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
@@ -34,14 +123,14 @@ defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool 
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
-# Enable tap to click. (Don't have to press down on the trackpad -- just tap it.)
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+# Tabs settings
 
-# Enable "tap-and-a-half" to drag.
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Dragging -int 0
-defaults write com.apple.AppleMultitouchTrackpad Dragging -int 0
+# Open pages in tabs instead of windows: automatically
+defaults write com.apple.Safari TabCreationPolicy -int 1
 
-# Enable 3-finger drag. (Moving with 3 fingers in any window)
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
-defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+# Update extensions automatically
+defaults write com.apple.Safari InstallExtensionUpdatesAutomatically -bool true
+
+
+# Use AirDrop over every interface. srsly this should be a default.
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
